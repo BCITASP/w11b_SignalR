d2f941afd05567a8bd1e3addcedf0c1635910303
alan-cy-lai <alan@ergostratus.com>
2015-03-27 10:42:46 -0700

    Initial Commit, Created Startup and Hub page

---
+++ HitCounter/HitCounterHub.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace HitCounter
+{
+    public class HitCounterHub : Hub
+    {
+        static int _hitCount = 0;
+
+        public void RecordHit()
+        {
+            _hitCount += 1;
+            Clients.All.onRecordHit(_hitCount);
+        }
+
+    }
+}
